1d685c824a81716068cbd9a36b53ed7bd6b01c7f
dolk13 <litvss@gmail.com>
2016-02-18 23:45:10 +0300

    Work with getopt

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
     cout << "Hello, World!" << endl;
+    std::string ip;
+    std::string path;
+    int port = 0;
+    int option = 0;
+    while ((option = getopt(argc, argv, "hpd")) != -1)
+    {
+        switch (option) {
+            case 'h' : {
+                std::cout << optarg << endl;
+                break;
+            }
+            case 'p' : {
+//                std::cout << optarg << endl;
+                port = atoi(optarg);
+                break;
+            }
+            case 'd' : {
+                std::cout << optarg << endl;
+                break;
+            }
+            default:
+                exit(EXIT_FAILURE);
+        }
+    }
+    cout << "port: " <<  port << endl;
     return 0;
 }
